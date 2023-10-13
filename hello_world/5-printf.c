83daa410f163fd642172d8a46bd34381cb6bed8c
saadikhemchane <saadi.khemchane@hotmail.com>
2023-10-13 02:08:29 -0700

    Hello, printf.

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art,, followed by a new line.
+ *
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
