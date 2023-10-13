d156775de7be798e9b2ac043a80072ac322076f1
saadikhemchane <saadi.khemchane@hotmail.com>
2023-10-13 01:26:15 -0700

    Hello, puts.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual
+ *        puzzle, followed by a new line.
+ *
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
