ed1340e0a36954cc468081efdba634fb98636868
saadikhemchane <saadi.khemchane@hotmail.com>
2023-11-14 00:32:40 -0800

    A new type dog_t as a new name for the type struct dog.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,20 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/* function prototypres */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
