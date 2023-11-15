e49c221d56db56e8cf1a687c47e6d16a3a3362fa
saadikhemchane <saadi.khemchane@hotmail.com>
2023-11-15 01:13:19 -0800

    Update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,56 +1,7 @@
 #include <stdlib.h>
+/* Pour la fonction strdup */
+#include <string.h>
 #include "dog.h"
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-
-/**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int len, i;
-
-	len = 0;
-
-	while (src[len] != '\0')
-	{
-		len++;
-	}
-
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
 
 /**
  * new_dog - creates a new dog
@@ -63,32 +14,31 @@ char *_strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	int len1, len2;
+	/* Allouer la mémoire pour la structure */
+	dog_t *dog = malloc(sizeof(dog_t));
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
-
-	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
 		return (NULL);
+	}
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
+	/* Utiliser strdup pour copier les chaînes de caractères */
+	dog->name = strdup(name);
+	dog->owner = strdup(owner);
+
+	/* Vérifier si l'allocation de mémoire a réussi */
+	if (dog->name == NULL || dog->owner == NULL)
 	{
-		free(dog);
-		return (NULL);
-	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
-	{
-		free(dog);
+		/* Libérer la mémoire allouée en cas d'échec */
 		free(dog->name);
+		free(dog->owner);
+		free(dog);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
+
+	/* Initialiser les autres champs de la structure */
 	dog->age = age;
 
+	/* Retourner le pointeur vers la nouvelle structure */
 	return (dog);
 }
