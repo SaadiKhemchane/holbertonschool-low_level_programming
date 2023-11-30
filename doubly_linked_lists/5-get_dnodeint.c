634b74e95aece57079f8c5b8d63f1fb001a22087
saadikhemchane <saadi.khemchane@hotmail.com>
2023-11-30 02:12:21 -0800

    correct my variable declaration

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node = NULL;
 	unsigned int count = 0;
 
+	node = head;
 	while (head == NULL)
 	{
 		return (NULL);
