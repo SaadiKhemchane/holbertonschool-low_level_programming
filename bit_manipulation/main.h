2edd30b75be199fd05ef84316255b95a1b5cf5b0
saadikhemchane <saadi.khemchane@hotmail.com>
2023-11-30 04:05:26 -0800

    The prototypes of all my functions

---
+++ bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+#endif
